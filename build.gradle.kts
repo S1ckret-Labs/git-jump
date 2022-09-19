1e261cf5c948422316488ab4ab0f5f679e44678b
Ivan Kovalenko <s1ckretbox@gmail.com>
2022-09-19 13:27:00 +0300

    build: add uberJar build
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,3 +27,27 @@ tasks.withType<KotlinCompile> {
 application {
     mainClass.set("com.s1ckret.labs.MainKt")
 }
+
+val uberJar = tasks.register<Jar>("uberJar") {
+    this.group = "build"
+    this.duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    archiveClassifier.set("uber")
+
+    manifest {
+        attributes["Main-Class"] = application.mainClass
+    }
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+    with(tasks.jar.get() as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(uberJar)
+    }
+}
